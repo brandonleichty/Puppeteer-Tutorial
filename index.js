bc98904e0e0e5be496b5b749eb47401fd59e8ad5
Brandon Leichty <brandonleichty@Brandons-MacBook-Pro.local>
2017-12-26 21:36:19 -0600

    Added selectors

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const CREDS = require('./creds');
+
+// Dependancies
 const puppeteer = require('puppeteer');
+
+
+const USERNAME_SELECTOR = '<input autocapitalize="off" autocorrect="off" autofocus="autofocus" class="form-control input-block" id="login_field" name="login" tabindex="1" type="text">'
+const PASSWORD_SELECTOR = '<input class="form-control form-control input-block" id="password" name="password" tabindex="2" type="password">';
+const BUTTON_SELECTOR = '<input class="btn btn-primary btn-block" data-disable-with="Signing in…" name="commit" tabindex="3" value="Sign in" type="submit">'
 
 async function run() {
     const browser = await puppeteer.launch({headless: false});
